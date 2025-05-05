d360a0d63727a24d8fb90205d0b61823a03da5da
falcaob <irene.falcaob@gmail.com>
2025-05-05 20:36:49 +0200

    add primeng and aura theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'mi-pagina', 
+        component: HomeComponent 
+    },
+    { 
+        path: '',
+        redirectTo: '/mi-pagina', 
+        pathMatch: 'full' 
+    }
+];
