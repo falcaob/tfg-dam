1df63bdf78ad1742bea18bacfe3bb9cc2ed62f21
falcaob <irene.falcaob@gmail.com>
2025-05-12 19:40:10 +0200

    finished header styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { 
-        path: 'mi-pagina', 
-        component: HomeComponent 
-    },
+
     { 
         path: '',
-        redirectTo: '/mi-pagina', 
+        redirectTo: '/home', 
         pathMatch: 'full' 
-    }
+    },
+    { 
+        path: 'home', 
+        component: HomeComponent 
+    },
+    
 ];
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';  
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MenubarModule, InputTextModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+
+  items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Competencias', routerLink: '/competencias' },
+      { label: 'Referentes AP', routerLink: '/referentes-ap' },
+      { label: 'Referentes AH', routerLink: '/referentes-ah' },
+      { label: 'Contacto', routerLink: '/contacto'}
+    ];
+  }
+
+}
