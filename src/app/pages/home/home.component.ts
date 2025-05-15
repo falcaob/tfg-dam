0ca4d8e461a5a2b9a22690549cf7117dcee35157
falcaob <irene.falcaob@gmail.com>
2025-05-15 19:34:02 +0200

    fixed styles homeComponent's p-card elements

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, RouterLink],
+  imports: [ButtonModule, RouterLink, CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
