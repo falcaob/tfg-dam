6a7fb40d73c163a06424d16e5f56d907dd4d313e
falcaob <irene.falcaob@gmail.com>
2025-05-19 18:28:59 +0200

    finished CompetenciasComponent

--- src/app/pages/competencias/competencias.component.ts
+++ src/app/pages/competencias/competencias.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-competencias',
   standalone: true,
-  imports: [CardModule, RouterLink],
+  imports: [CardModule, RouterLink, AccordionModule, ButtonModule],
   templateUrl: './competencias.component.html',
   styleUrl: './competencias.component.css'
 })
